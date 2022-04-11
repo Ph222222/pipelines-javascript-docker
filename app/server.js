11afd53735e6ab867f8c993ac4147f21842d944d
Philip Belder <philipb@avanan.com>
2022-04-11 12:41:45 +0300

    adding async

--- app/server.js
+++ app/server.js
@@ -5,12 +5,13 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-function fibo(n) { 
-
-  console.log("N is:" + n)
+function fibo(n,num) { 
+  if (n > 16) {
+   console.log("N is:" + n + "Num is: " + num)
+  }
   if (n < 2)
       return 1;
-  else   return fibo(n - 2) + fibo(n - 1);
+  else   return fibo(n - 2, num) + fibo(n - 1, num);
 }
 
 
@@ -18,7 +19,12 @@ const app = express();
 app.get('/', (req, res) => {
   let num = parseInt(req.headers.fibo); 
   console.log(num)
-  res.send('Hello world ' + num);
+  //fibo(num, num);
+  setTimeout(() => {
+    fibo(num, num);
+  }, 1000);
+  
+  res.send('Processing ' + num);
 });
 
 app.listen(PORT, HOST);
--- request.py
+++ request.py
@@ -1,3 +1,9 @@
+from sre_constants import RANGE
+from time import sleep
 import requests
-r=requests.get("http://0.0.0.0:8080", headers={"fibo":"12"})
-print (r.content)
+
+
+for n in range (40, 50):
+    r=requests.get("http://0.0.0.0:8080", headers={"fibo": str(n)})
+    print (r.content)
+    #sleep(5)
